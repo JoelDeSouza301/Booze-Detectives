7bf6adcb60e089870053f92869f06c938245a6ce
JoelDeSouza301 <jdso120@aucklanduni.ac.nz>
2024-12-03 13:39:59 +1300

    initial-project -setup

---
+++ booze-detectives/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hey Guys we are Booze Detectives!!
+        </p>
+        <a
+          className="App-link"
+          href="https://www.google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Welcome to our page
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
